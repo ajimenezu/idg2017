f373ae87263a6f738618b30c4d09cfe0208c9624
GabrielPerezB <gabrielapb1996@gmail.com>
2017-07-18 17:10:40 -0600

    create project

--- AppPrueba/app/src/main/java/com/example/root/appprueba/MainActivity.java
+++ AppPrueba/app/src/main/java/com/example/root/appprueba/MainActivity.java
@@ -2,12 +2,52 @@ package com.example.root.appprueba;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 
 public class MainActivity extends AppCompatActivity {
+
+    Switch luzSwitch, puertaSwitch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        luzSwitch = (Switch) findViewById(R.id.switchLuces);
+        puertaSwitch = (Switch) findViewById(R.id.switchPuertas);
+
+        luzSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+
+
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+                if(isChecked){
+                    System.out.println(" luz haha");
+                }else{
+                    System.out.println("luz hoho");
+                }
+
+            }
+        });
+
+        puertaSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+
+
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+                if(isChecked){
+                    System.out.println(" puerta haha");
+                }else{
+                    System.out.println("puerta hoho");
+                }
+
+            }
+        });
     }
+
+
+
+
 }
