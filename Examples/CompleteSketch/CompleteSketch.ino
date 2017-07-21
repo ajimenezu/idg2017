4d86f052d27833583262de60a5c5010acf709882
ajimenezu <leiver38hfh@gmail.com>
2017-07-21 15:35:50 -0600

    Sketch completo

--- Examples/CompleteSketch/CompleteSketch.ino
+++ Examples/CompleteSketch/CompleteSketch.ino
@@ -6,7 +6,7 @@
 #include <EthernetServer.h>
 #include <EthernetUdp.h>
 #include <SPI.h>
-
+#include <Servo.h>  // servo library
 //-----------------------------------Ethernet Shield---------------------------------------#
 EthernetClient client = EthernetClient();
 byte mac[] = {0x90, 0xA2, 0xDA, 0x0E, 0x98, 0x91};
@@ -14,8 +14,18 @@ char server[] = "idg2017.herokuapp.com";
 
 //-----------------------------------Led's-------------------------------------------------#
 const int led = 3;  
-//-----------------------------------Setup Method------------------------------------------#
 
+//-----------------------------------Servo-------------------------------------------------#
+Servo lockDoor; //4
+
+//-----------------------------------Timbre------------------------------------------------#
+const int timbre = 5;
+const int buzzer = 6;
+
+//-----------------------------------Timbre------------------------------------------------#
+const int temperatura = A0;
+
+//-----------------------------------Setup Method------------------------------------------#
 void setup() {
   // Serial Connection
   Serial.begin(9600);
@@ -25,14 +35,61 @@ void setup() {
   Serial.println(Ethernet.localIP()); // Shows the IP Address
 
   // Led's
-  pinMode(led,INPUT);
+  pinMode(led,OUTPUT);
 
+  // Servo
+  lockDoor.attach(4,900,2100);  // Puerto 4
+
+  //Timbre
+  pinMode(timbre,INPUT);
+  pinMode(buzzer,OUTPUT);
+}
+float getVoltage(int pin) 
+{
+  return (analogRead(pin) * 0.004882814); 
+}
+//-----------------------------------Door Method-------------------------------------------#
+void Termometro(){
+  float voltage, degreesC;
+  voltage = getVoltage(temperatura);  //  Obtiene el voltaje del pin Analogico 
+  degreesC = (voltage - 0.5) * 100.0; //  Convierte el voltaje a grados Celsius
+  if(client.connect(server,80)){  //  Si hay conexión al API
+    client.println("POST /AddTemperatura?ID=0&Dato="+String(degreesC)+" HTTP/1.1"); // Consulta al API-REST #cambiar ID
+    client.println("Host: idg2017.herokuapp.com"); // Host al que se consulta
+    client.println();
+    client.stop();  //  Se detiene la conexión al API
+    Serial.println(degreesC);
+  }else{
+    Serial.println("Error de conexion");
+  }
+}
+/*
+//-----------------------------------Door Method-------------------------------------------#
+void Timbre(){
+  if(digitalRead(timbre) == LOW){
+    tone(buzzer,'c',113);
+    delay(113);
+    Serial.println("Sending..");
+    if(client.connect(server,80)){  //  Si hay conexión al API
+      client.println("POST /UpdateTimbre?ID=0&Estado=1 HTTP/1.1"); // Consulta al API-REST #cambiar ID
+      client.println("Host: idg2017.herokuapp.com"); // Host al que se consulta
+      client.println();
+      client.stop();  //  Se detiene la conexión al API
+    }else{
+      Serial.println("Error de conexion");
+    }
+    Serial.println("Done..");
+    //PITA
+    tone(buzzer,'a',113);
+    delay(113);
+  }
 }
 
-void Luces(){
+//-----------------------------------Door Method-------------------------------------------#
+void Puerta(){
   String mensaje = "";
-  if(client.connect(server,80)){
-    client.println("POST /GetLuz?ID=0 HTTP/1.1"); // Consulta al API-REST
+  if(client.connect(server,80)){  //  Si hay conexión al API
+    client.println("POST /GetPuerta?ID=0 HTTP/1.1"); // Consulta al API-REST #cambiar ID
     client.println("Host: idg2017.herokuapp.com"); // Host al que se consulta
     client.println();
     while(client.connected() && !client.available()) delay(1);  // Espera a que el API este disponible
@@ -41,12 +98,12 @@ void Luces(){
       mensaje.concat(c);
     }
     client.stop();  // Se detiene la conexión al API
-    Serial.println(mensaje.substring(173,174));
+    Serial.println("Puerta: "+mensaje.substring(173,174));
     if (mensaje.substring(173,174) == "1"){
-      Serial.println("Encendido");
+      lockDoor.write(0);
       digitalWrite(led,HIGH);
-    }else{xx
-      Serial.println("Apagado");
+    }else{
+      lockDoor.write(90);
       digitalWrite(led,LOW);
     }
   }else{
@@ -54,9 +111,37 @@ void Luces(){
   }
 }
 
-
-
+//-----------------------------------Light Method-----------------------------------------#
+void Luz(){
+  String mensaje = "";
+  if(client.connect(server,80)){  //  Si hay conexión al API
+    client.println("POST /GetLuz?ID=0 HTTP/1.1"); // Consulta al API-REST #Cambiar ID
+    client.println("Host: idg2017.herokuapp.com"); // Host al que se consulta
+    client.println();
+    while(client.connected() && !client.available()) delay(1);  // Espera a que el API este disponible
+    while (client.connected() && client.available()){
+      char c =  client.read();  // Lee la respuesta del API
+      mensaje.concat(c);
+    }
+    client.stop();  // Se detiene la conexión al API
+    Serial.println("Luz: "+(mensaje.substring(173,174)));
+    if (mensaje.substring(173,174) == "1"){
+      Serial.println("Encendido");
+      digitalWrite(led,HIGH);
+    }else{
+      Serial.println("Apagado");
+      digitalWrite(led,LOW);
+    }
+  }else{
+    Serial.println("No se concretó la conexion");
+  }
+}
+*/
+//-----------------------------------Loop Method------------------------------------------#
 void loop() {
-  Luces();
-  delay(2000);
+  //Luz();
+  //Puerta();
+  //Timbre();
+  Termometro();
+  delay(5000);
 }
